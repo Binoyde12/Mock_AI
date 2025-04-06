a4e45fb3c5fa6e9090a9f375e60ca5b964b5714b
Binoyde12 <babuldebnath0109@gmail.com>
2025-04-07 02:23:13 +0530

    Auth & Rout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+
+import { BrowserRouter as Router ,Routes,Route } from 'react-router-dom'
+import { PublicLayout } from '@/layouts/public-layout'
+import HomePage from '@/routes/home'
+import AuthenticationLayout from '@/layouts/auth-layout'
+import { SignInPage } from './routes/sign-in'
+import { SignUpPage } from './routes/sign-up'
+import ProtectedRoutes from './layouts/protected-routes'
+import { MainLayout } from './layouts/main-layout'
 
 
 const App = () => {
   return (
-    <div className='text-blue-500'>
-        App
+    <div >
+      <Router>
+        <Routes>
+             {/* Add your routes here */}
+             <Route element={<PublicLayout/>}>
+                 <Route index element={<HomePage/>}/>
+             </Route>
+
+             {/*authentication layout*/}
+             <Route element={<AuthenticationLayout/>}>
+                <Route path='/signin/*' element={<SignInPage/>}/>
+                <Route path='/signup/*' element={<SignUpPage/>}/>
+             </Route>
+             {/* protected routes */}
+             <Route 
+                element={<ProtectedRoutes>
+                <MainLayout/>
+                </ProtectedRoutes>}>
+
+
+              {/* Add all protected routes*/}
+             </Route>
+        </Routes>
+      </Router>
+        
     </div>
     
   )
