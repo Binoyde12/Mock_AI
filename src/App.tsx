2b76e50a260d3d81deab6de70b6eda3cbf0f7efb
Binoyde12 <babuldebnath0109@gmail.com>
2025-04-07 23:49:39 +0530

    MockAI Completed

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ import { SignInPage } from './routes/sign-in'
 import { SignUpPage } from './routes/sign-up'
 import ProtectedRoutes from './layouts/protected-routes'
 import { MainLayout } from './layouts/main-layout'
+import { Generate } from './components/generate'
+import { Dashboard } from './routes/dashboard'
+import { CreateEditPage } from './routes/create-edit-page'
+import { MockLoadPage } from './routes/mock-load-page'
+import { MockInterviewPage } from './routes/mock-interview-page'
+import { Feedback } from './routes/feedback'
 
 
 const App = () => {
@@ -32,6 +38,15 @@ const App = () => {
 
 
               {/* Add all protected routes*/}
+              <Route element={<Generate/>} path='/generate'>
+                  <Route index element ={<Dashboard/>}/>
+                  <Route path=":interviewId" element ={<CreateEditPage/>}/>
+                  <Route path="interview/:interviewId" element ={<MockLoadPage/>}/>
+                  <Route path="interview/:interviewId/start" element ={<MockInterviewPage/>}/>
+                  
+                  <Route path="feedback/:interviewId" element ={<Feedback/>}/>
+
+              </Route>
              </Route>
         </Routes>
       </Router>
